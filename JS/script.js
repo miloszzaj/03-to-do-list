3e0d3a455fcadb185e58175a9ee8b35de4af728f
Milosz Zajaczkowski <zajaczkowski.milosz@gmail.com>
2022-01-04 18:22:43 +0100

    addingNewTask added in init()

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,40 @@
-const addingTest = (task, listTasks, newTask) => {
-  listTasks.innerHTML += `<li>${task}</li>`;
+// const addingTest = (task, listTasks, newTask) => {
+//   listTasks.innerHTML += `<li>${task}</li>`;
 
-  console.log(task);
-};
+//   console.log(task);
+// };
+
+const tasks = [{ content: "" }];
 
 const addingNewTask = e => {
   e.preventDefault();
-  const listTasks = document.querySelector(".js-list__tasks");
-  const tasks = [{ x: "aa" }];
+  const inputElement = document.querySelector(".js-addingInput");
+  let task = inputElement.value.trim();
+  if (task === "") {
+    return;
+  }
+
+  tasks.push({
+    content: task,
+  });
+
+  renderFromArray();
+};
+
+const renderFromArray = () => {
+  let listTasks = document.querySelector(".js-list__tasks");
 
   let newTask = "";
 
   for (const task of tasks) {
-    newTask += `<li>${task.x}</li>`;
+    newTask += `<li>${task.content}</li>`;
   }
 
   listTasks.innerHTML = newTask;
-  //   console.log(tasks);
-  const inputElement = document.querySelector(".js-addingInput");
-  const task = inputElement.value.trim();
-  addingTest(task, listTasks);
 };
-
-const render = () => {};
 
 const init = () => {
   const addButtonElement = document.querySelector(".js-addButton");
   addButtonElement.addEventListener("click", e => addingNewTask(e));
-
-  render();
 };
 init();
